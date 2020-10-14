31bbacefbfa614999d0a745e26b76422704fc997
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-10-14 19:34:34 +0200

    Style Input component

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './style.css';
 
 const Input = ({type, placeholder, setValue}) => {
     return (
         <input
+            className="input"
             type={type}
             placeholder={placeholder}
             onChange={(event) => setValue(event.target.value)}
