b246579492e29156eef4d4b786c35f955dfba1ba
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-10-14 16:39:36 +0200

    Fix requests being sent multiple times

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,11 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Button, Select} from '../';
 
 const Calculator = () => {
     const [amount, setAmount] = useState(0);
+    const [currencies, setCurrencies] = useState([]); // 'AUD', 'BGN', ...
     const [currencyFrom, setCurrencyFrom] = useState('PLN');
     const [currencyTo, setCurrencyTo] = useState('USD');
     const [result, setResult] = useState(0);
+
+    useEffect(() => {
+        fetch(`https://api.ratesapi.io/api/latest?base=PLN`)
+        .then(res => res.json())
+        .then(data => {
+            setCurrencies(Object.keys(data.rates));
+        })
+        .catch(err => console.log(err));
+
+        return () => {
+            //clearInterval
+            //removeEventListener
+        }
+    }, []);
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -28,11 +43,11 @@ const Calculator = () => {
             </div>
             <div>
                 <span className="label">From:</span>
-                <Select value={currencyFrom} setCurrency={setCurrencyFrom} />
+                <Select value={currencyFrom} setCurrency={setCurrencyFrom} currencies={currencies} />
             </div>
             <div>
                 <span className="label">To:</span>
-                <Select value={currencyTo} setCurrency={setCurrencyTo} />
+                <Select value={currencyTo} setCurrency={setCurrencyTo} currencies={currencies} />
             </div>
             <div>
                 <span className="result">Result: {result.toFixed(2)}</span>
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
-const Select = ({value, setCurrency}) => {
+const Select = ({value, setCurrency, currencies}) => {
     return (
         <select value={value} onChange={(event) => setCurrency(event.target.value)}>
-            <option value="USD">USD</option>
-            <option value="PLN">PLN</option>
-            <option value="EUR">EUR</option>
-            <option value="GBP">GBP</option>
+            {currencies.map((element) => <option key={`curr-${element}`} value={element}>{element}</option>)}
         </select>
     );
 }
