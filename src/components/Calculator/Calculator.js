8b77064bbe7f0e422502b6d34d54298a236b10ba
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-10-14 15:42:13 +0200

    Correct style in Button

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,13 +1,18 @@
 import React, {useState} from 'react';
-import {Select} from '../';
+import {Button, Select} from '../';
 
 const Calculator = () => {
     const [amount, setAmount] = useState(0);
     const [currencyFrom, setCurrencyFrom] = useState('PLN');
     const [currencyTo, setCurrencyTo] = useState('USD');
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        //https://api.ratesapi.io/api/latest?base=PLN
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <input
                     type="number"
@@ -15,19 +20,19 @@ const Calculator = () => {
                     onChange={(event) => setAmount(event.target.value)}
                 />
             </div>
-
             <div>
                 <span className="label">From:</span>
                 <Select value={currencyFrom} setCurrency={setCurrencyFrom} />
             </div>
-
             <div>
                 <span className="label">To:</span>
                 <Select value={currencyTo} setCurrency={setCurrencyTo} />
             </div>
-
             <div>
                 <span className="result">Result: {amount}</span>
+            </div>
+            <div>
+                <Button>Send</Button>
             </div>
         </form>
     );
