40afa5c376c442fec0a01121ba92a46b94f1e47e
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-10-14 18:39:08 +0200

    'About' page change

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import {useRouteMatch, useParams} from 'react-router-dom';
 
 const About = () => {
-    let match = useRouteMatch();
-    let params = useParams();
-
-    console.log('match:', match);
-    console.log('params:', params);
-
     return (
         <h2>About page</h2>
     );
