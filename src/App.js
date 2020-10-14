b35700ad7bb95f307c81b4280a00eb8fdc85af29
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-10-14 17:08:15 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom';
 import Calculator from './pages/Calculator';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Currency Converter</h1>
-      <Calculator />
+      <Router>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+
+        <Switch>
+          {/* <Route path="/" component={Calculator} />
+          for class component (works for functional as well though) */}
+          <Route path="/" exact>
+            <Calculator />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
