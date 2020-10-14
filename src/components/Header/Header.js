63dde3bd9f5712ac9356dc8e02f6f6e70b300808
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-10-14 19:17:28 +0200

    Style Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Menu} from '../';
+import './style.css';
 
 const Header = ({pages}) => {
     return (
         <header className="App-header">
-            <h1>Currency converter</h1>
+            <h2>Currency converter</h2>
             <Menu pages={pages} />
         </header>
     );
