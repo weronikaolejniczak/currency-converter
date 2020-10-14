00a4df455984ae64dc0052587cdba328bfa8c5f9
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-10-14 15:28:58 +0200

    Hooks in Calculator Component

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Calculator = () => {
+    const [amount, setAmount] = useState(0);
+    const [currencyFrom, setCurrencyFrom] = useState('PLN');
+    const [currencyTo, setCurrencyTo] = useState('USD');
+
     return (
         <form>
         <div>
           <input
             type="number"
             placeholder="Amount"
-            onChange={(event) => {console.log(event.target.value)}}
+            onChange={(event) => setAmount(event.target.value)}
           />
         </div>
         <div>
           <span className="label">From:</span>
-          <select onChange={(event) => {console.log(event.target.value)}}>
+          <select value={currencyFrom} onChange={(event) => setCurrencyFrom(event.target.value)}>
             <option value="USD">USD</option>
             <option value="PLN">PLN</option>
             <option value="EUR">EUR</option>
@@ -21,7 +25,7 @@ const Calculator = () => {
         </div>
         <div>
         <span className="label">To:</span>
-          <select onChange={(event) => {console.log(event.target.value)}}>
+          <select value={currencyTo} onChange={(event) => setCurrencyTo(event.target.value)}>
             <option value="USD">USD</option>
             <option value="PLN">PLN</option>
             <option value="EUR">EUR</option>
@@ -29,7 +33,7 @@ const Calculator = () => {
           </select>
         </div>
         <div>
-            <span className="result">Result:</span>
+            <span className="result">Result: {amount}</span>
         </div>
       </form>
     );
