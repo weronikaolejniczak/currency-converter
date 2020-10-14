b50fdc1cfb5dc33c46f97dcbc2109e2032abd10b
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-10-14 15:12:10 +0200

    Calculator onChange handler

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Calculator} from './components';
+import {Calculator} from './components';
 import './App.css';
 
 function App() {
--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -4,11 +4,15 @@ const Calculator = () => {
     return (
         <form>
         <div>
-          <input type="number" placeholder="Amount" />
+          <input
+            type="number"
+            placeholder="Amount"
+            onChange={(event) => {console.log(event.target.value)}}
+          />
         </div>
         <div>
-          <span>From:</span>
-          <select>
+          <span className="label">From:</span>
+          <select onChange={(event) => {console.log(event.target.value)}}>
             <option value="USD">USD</option>
             <option value="PLN">PLN</option>
             <option value="EUR">EUR</option>
@@ -16,13 +20,16 @@ const Calculator = () => {
           </select>
         </div>
         <div>
-        <span>To:</span>
-          <select>
+        <span className="label">To:</span>
+          <select onChange={(event) => {console.log(event.target.value)}}>
             <option value="USD">USD</option>
             <option value="PLN">PLN</option>
             <option value="EUR">EUR</option>
             <option value="GBP">GBP</option>
           </select>
+        </div>
+        <div>
+            <span className="result">Result:</span>
         </div>
       </form>
     );
