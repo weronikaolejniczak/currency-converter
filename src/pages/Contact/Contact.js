d41454285ca9f227dd60925abf510ec060b09126
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-10-15 15:28:21 +0200

    Style

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import './style.css';
 
 const Contact = () => {
     return (
-        <h2>Contact page</h2>
+        <div className="App-Contact">
+            <div className="contact-container">
+                <h2>Contact page</h2>
+                <p>Lorem Ipsum is the single greatest threat. We are not - we are not keeping up with other websites. Lorem Ipsum best not make any more threats to your website. It will be met with fire and fury like the world has never seen. Does everybody know that pig named Lorem Ipsum? An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud.</p>
+            </div>
+        </div>
     );
 }
 
