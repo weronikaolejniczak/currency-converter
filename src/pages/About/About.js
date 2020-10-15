94df59f45cb67fee620c34bc6394ea34fd514762
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-10-15 15:42:01 +0200

    Style and About page content

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,10 +4,16 @@ import './style.css';
 const About = () => {
     return (
         <div className="App-About">
-            <div className="about-container">
+            <section className="about-container">
                 <h2>About page</h2>
-                <p>Lorem Ipsum is the single greatest threat. We are not - we are not keeping up with other websites. Lorem Ipsum best not make any more threats to your website. It will be met with fire and fury like the world has never seen. Does everybody know that pig named Lorem Ipsum? An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud.</p>
-            </div>
+                <p>Hi! My name is <b>Weronika</b> and I'm a front end developer.</p>
+
+                <p>This is a <b>React</b> project with styling in <b>CSS</b>, <b>API data fetching</b> and simple page routing with <b>React Router</b>.</p>
+
+                <p>Credits to <a href="https://github.com/patrykomiotek" alt="Patryk Omiotek @GitHub" rel="noopener noreferrer" target="_blank">@patrykomiotek</a> from <a href="https://szkolareacta.pl" alt="Szkola Reacta" rel="noopener noreferrer" target="_blank">Szkola Reacta</a>.</p>
+
+                <p>Check out original code at <a href="https://github.com/patrykomiotek/workshops-currency-converter" alt="Patryk Omiotek @GitHub" rel="noopener noreferrer" target="_blank">workshops-currency-converter</a>.</p>
+            </section>
         </div>
     );
 }
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
         <div className="App-Contact">
             <div className="contact-container">
                 <h2>Contact page</h2>
-                <p>Lorem Ipsum is the single greatest threat. We are not - we are not keeping up with other websites. Lorem Ipsum best not make any more threats to your website. It will be met with fire and fury like the world has never seen. Does everybody know that pig named Lorem Ipsum? An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud.</p>
+                <p>Lorem ipsum dolor amet mustache knausgaard +1, blue bottle waistcoat tbh semiotics artisan synth stumptown gastropub cornhole celiac swag. Brunch raclette vexillologist post-ironic glossier ennui XOXO mlkshk godard pour-over blog tumblr humblebrag. Blue bottle put a bird on it twee prism biodiesel brooklyn. Blue bottle ennui tbh succulents.</p>
             </div>
         </div>
     );
